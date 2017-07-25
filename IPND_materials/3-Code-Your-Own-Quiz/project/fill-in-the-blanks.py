93d1ba2d250cd9b42a77f5580c8fecacc570a287
Evan Daly <daly.evan@gmail.com>
2017-07-25 15:30:43 -0700

    Uses GameFifficulty levels as Enum type for ?easier? referencing

--- IPND_materials/3-Code-Your-Own-Quiz/project/fill-in-the-blanks.py
+++ IPND_materials/3-Code-Your-Own-Quiz/project/fill-in-the-blanks.py
@@ -30,18 +30,44 @@ from enum import Enum
 # How can you adapt that design to work with numbered blanks?
 
 # If you need help, you can sign up for a 1 on 1 coaching appointment: https://calendly.com/ipnd-1-1/20min/
+gui_bar = '='*50
+
+global_debug = True
+
+
+def DebugMessage(message=""):
+    """Displays a debug message if the global variable 'global_debug' is true"""
+    if global_debug:
+        print("Debug_Message: "+ message)
+    return 0
 
 
 class GameDifficulty(Enum):
-    BEGINNER = 0
-    MEDIUM = 1
-    HARD = 2
+    """Defines game difficulty levels for QASet()
+    https://docs.python.org/3/library/enum.html"""
+    Easy = 1
+    Medium = 2
+    Hard = 3
 
 
-def game_type(difficulty = GameDifficulty.BEGINNER):
-    """Returns a string and answer set used for each game difficulty type"""
+def QASet(game):
+    """Contains the Question and Answers set for each game dificulty.  Need to define a difficulty to execute"""
+    DebugMessage(f"{QASet.__name__ },  game :{game}")
+    game_difficulty = game.name
 
-    if difficulty == GameDifficulty.BEGINNER:
+    if game_difficulty == GameDifficulty.Easy.name:
+        text = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
+            adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
+            don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
+            tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
+
+        answers = {'___1___': 'function',
+                   '___2___': 'variable',
+                   '___3___': 'None',
+                   '___4___': 'something'
+                   }
+        return text, answers
+    elif game_difficulty == GameDifficulty().Medium.name:
         text = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
             adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
             don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
@@ -53,20 +79,7 @@ def game_type(difficulty = GameDifficulty.BEGINNER):
                    '___4___': 'something'
                    }
         return text, answers
-    elif difficulty == GameDifficulty.MEDIUM:
-        text = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
-            adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
-            don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
-            tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
-
-        answers = {'___1___': 'functions',
-                   '___2___': 'something',
-                   '___3___': 'something',
-                   '___4___': 'something'
-                   }
-        return text, answers
-
-    elif difficulty == GameDifficulty.HARD:
+    elif game_difficulty == GameDifficulty().Hard.name:
         text = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
             adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
             don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
@@ -79,4 +92,64 @@ def game_type(difficulty = GameDifficulty.BEGINNER):
                    }
         return text, answers
     else:
-        return
+        return -1
+
+
+def game_menu(question="Hello", option=["Test"]):
+    """Menu which takes a message [question] with options list [options] and returns the users selected option.
+        Quit is always an option"""
+    DebugMessage(game_menu.__name__ + " is running")
+    number_of_options = len(option)
+    # Check if options list and question is empty
+    if number_of_options > 0:
+        option.append("Quit")
+        while True:
+            print(question)
+            print("Options:"+ str(option))
+            response = input(">")
+
+            if response.lower() == option[number_of_options].lower():
+                print("Good bye!")
+                exit(0)
+
+            for opt in option:
+                if opt.lower() == response.lower():
+                    print("Valid Response")
+                    print(opt)
+                    return opt
+
+            print(f"{response}, is not a valid option")
+            print(gui_bar)
+
+    else:
+        return -1
+
+
+def game_main():
+    """Game Session starts with user slelecting game difficulty level"""
+    DebugMessage(game_main.__name__ + " is running")
+    DebugMessage("Game Menu started")
+    #Loop though game menu till user quits
+    while True:
+        print("\r\r")
+        print("Would you like to play a game?")
+
+        print(gui_bar)
+        GameLevel = game_menu("Pick a difficulty", levels)
+        gameSession(GameLevel)
+        print("\n\n")
+
+
+def game_session(game):
+    DebugMessage(game_session.__name__ + " is running")
+    #print("\n"*10)
+    print("Game Level: " + str(game))
+    print(gui_bar)
+    print(QASet(game))
+
+    pass
+gameLevels = (list(GameDifficulty.__members__))
+MyGame = GameDifficulty.Easy
+game_session(MyGame)
+
+print(gameLevels)
