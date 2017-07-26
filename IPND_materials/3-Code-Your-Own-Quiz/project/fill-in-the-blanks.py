1c71e797fd1e19dc43ef603f4c3ddce974cadfbe
Evan Daly <daly.evan@gmail.com>
2017-07-26 01:19:04 -0700

    GameDifficulty.Easy level loop completed

--- IPND_materials/3-Code-Your-Own-Quiz/project/fill-in-the-blanks.py
+++ IPND_materials/3-Code-Your-Own-Quiz/project/fill-in-the-blanks.py
@@ -1,4 +1,6 @@
 from enum import Enum
+# Enable debugging by setting 'global_debug = True'
+global_debug = False
 
 # IPND Stage 2 Final Project
 
@@ -31,10 +33,9 @@ from enum import Enum
 # How can you adapt that design to work with numbered blanks?
 
 # If you need help, you can sign up for a 1 on 1 coaching appointment: https://calendly.com/ipnd-1-1/20min/
-gui_bar = '=' * 50
+gui_bar = '=' * 49
 
-# Enable debugging by setting 'global_debug = True'
-global_debug = True
+
 
 
 def DebugMessage(message=""):
@@ -43,13 +44,20 @@ def DebugMessage(message=""):
     if global_debug:
         print("\033[93m DEBUG: " + message + "\033[0m")
 
+""" Method to use test values [not used]
+def DebugSetValue(variable_name, orig_value, new_value):
+    if global_debug:
+        DebugMessage(f"Setting {variable_name} from {orig_value} to {new_value}")
+        return new_value
+    return orig_value
+"""
 
-class GameDifficulty(Enum):
+class GameLevels(Enum):
     """Defines game difficulty levels Name and Values for QASet()
     https://docs.python.org/3/library/enum.html"""
-    Easy = 10
+    Easy = 1
     Medium = 5
-    Hard = 3
+    Hard = 2
 
 
 def menu_selection(question="Hello", option=[]):
@@ -65,7 +73,7 @@ def menu_selection(question="Hello", option=[]):
         DebugMessage(f"""def:displaymenu | option={option}""")
         print(question)
         print("Options:" + str(option))
-        response = input(">")
+        response = input("$> ")
 
         for opt in option:
             if response.lower() == opt.lower():
@@ -81,88 +89,137 @@ def menu_selection(question="Hello", option=[]):
 
 
 def game_main():
-    """Game Session starts with user slelecting game difficulty level"""
+    """Game Session starts with user selecting game difficulty level"""
     DebugMessage("""def:game_main""")
-    gamelevels = (list(GameDifficulty.__members__))
+    gamelevels = (list(GameLevels.__members__))
 
     # Loop though game menu till user quits
     while True:
-        print("\r\r")
-        print("Would you like to play a game?")
         print(gui_bar)
+        print(" "*13 + "FILL IN THE BLANKS GAME")
+        print(gui_bar)
+
         selection = menu_selection("Select a difficulty level:", gamelevels)
         game_session(selection)
 
 
-def quizitems(game_difficulty):
-    """Contains the Question and Answers set for each game difficulty."""
-    DebugMessage(f"""QASet, args: game_difficulty={game_difficulty}""")
+def quizitems(gamelevel):
+    """Contains the quiztext and answers set for each gamelevel."""
+    DebugMessage(f"""QASet, args: gamelevel={gamelevel}""")
 
-    if game_difficulty == GameDifficulty.Easy.name:
-        text = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
-            adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
-            don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
-            tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
+    # TODO Create unique game messages for each level
+
+    if gamelevel == GameLevels.Easy.name:
+        quiztext = '''  A {0} is created with the def keyword. You specify the inputs a {0} takes by
+adding {1} separated by commas between the parentheses. {0}s by default return {2} if you
+don't specify the value to return. {1} can be standard data types such as string, number, dictionary,
+tuple, and {3} or can be more complicated such as objects and lambda functions.'''
 
         answers = {'___1___': 'function',
                    '___2___': 'variable',
                    '___3___': 'None',
                    '___4___': 'something'
                    }
-        attempts = GameDifficulty.Easy.value
-        return text, answers, attempts
-    elif game_difficulty == GameDifficulty.Medium.name:
-        text = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
-            adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
-            don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
-            tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
+        attempts = GameLevels.Easy.value
+        return quiztext, answers, attempts
+    elif gamelevel == GameLevels.Medium.name:
+        quiztext = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
+        adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
+        don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
+        tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
 
         answers = {'___1___': 'functions',
                    '___2___': 'something',
                    '___3___': 'something',
                    '___4___': 'something'
                    }
-        attempts = GameDifficulty.Medium.value
-        return text, answers, attempts
-    elif game_difficulty == GameDifficulty.Hard.name:
-        text = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
-            adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
-            don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
-            tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
-
-        answers = {'___1___': 'functions',
-                   '___2___': 'something',
-                   '___3___': 'something',
-                   '___4___': 'something'
+        attempts = GameLevels.Medium.value
+        return quiztext, answers, attempts
+    elif gamelevel == GameLevels.Hard.name:
+        answers = {'function': '___1___',
+                   'something0': '___2___',
+                   'something1': '___3___',
+                   'something2': '___4___'
                    }
-        attempts = GameDifficulty.Hard.value
-        return text, answers, attempts
+        quiztext = f'''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
+        adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
+        don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
+        tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
+
+        attempts = GameLevels.Hard.value
+        return quiztext, answers, attempts
     else:
         return -1
 
-def game_session(dificulty_level):
-    """The begging of a game"""
-    DebugMessage(f"def:game_session | game={dificulty_level}")
-    # print("\n"*10)
-    if dificulty_level not in GameDifficulty.__members__:
-        DebugMessage(f"Invalid game type received {dificulty_level}, exiting...")
+
+def quiz(fill_in_blank_text, blank_item, correct_answer):
+    """Boollean object that returns True if user guesses correctly for quizitem in fill_in_blank_text"""
+    print(fill_in_blank_text)
+    print("")
+    guess = input(f"What is your guess for {blank_item}?: ")
+
+    if guess.lower() == correct_answer.lower():
+        return True
+    return False
+
+
+def game_session(difficulty_level):
+    """Initializes the game based on selected level in GameDifficulty and retrieves the quiz from quizitems"""
+    DebugMessage(f"def:game_session | game={difficulty_level}")
+
+    # Verify correct parameters/values are passed in before continuing, exit for debugging
+    if difficulty_level not in GameLevels.__members__:
+        DebugMessage(f"Invalid game type received {difficulty_level}, exiting...")
         exit(1)
-    print("Game Level: " + str(dificulty_level))
+
+    # Initialize the game session with selected difficulty level.  Get quiz items for myGame variables.
+    myGame_FillInBlankText, myGame_answers, myGame_attempts = quizitems(difficulty_level)
+    # myGame_answers = list(myGame_answers)
+    # Fill-In-The-Blank Message in quizitem
+    DebugMessage(f"myGame_FillInBlankMessage= {myGame_FillInBlankText}")
+    # The Answers used to match blanks in message
+    DebugMessage(f"myGame_answers= {myGame_answers}")
+    # Attempts remaining based on GameDifficulty Enum values
+    DebugMessage(f"myGame_attempts= {myGame_attempts}")
+
+    print(f"Game Level: {difficulty_level}")
+
     print(gui_bar)
 
-    #
-    myGame_FillInBlankMessage, myGame_answers, myGame_attempts = quizitems(dificulty_level)
-    # Fill-In-The-Blank Message used in QuizItem
-    print(myGame_FillInBlankMessage)
-    # The Answers used to match blanks in message
-    print(myGame_answers)
-    # Attempts remaining based on GameDifficulty Enum values
-    print(myGame_attempts)
 
-    # TODO While myGameAttempts > 0 run game session
-    # TODO Game QuizItem checks for user input matching myGameAnswers to question(___#___) against answer
+    my_answer_list = list(myGame_answers.keys())
+    my_formatted_text = myGame_FillInBlankText.format(*my_answer_list)
+
+    index = 0
+    for key, val in myGame_answers.items():
+        DebugMessage("key={key}, val={val}")
+        print(f"Guesses Remaining: {myGame_attempts}")
+        print("")
+        # TODO Ask for user input matching to match the blank item with the correct answer
+        while not quiz(my_formatted_text, key, val):
+            myGame_attempts -= 1
+            print(f"Remaining guesses: {myGame_attempts}")
+            print("")
+            # TODO if remaining attempts is 0, end game session
+            if myGame_attempts <= 0:
+                print(".....Game Over, try again?")
+                print("\n\n")
+                return 0
+        my_answer_list[index] = val
+        my_formatted_text = myGame_FillInBlankText.format(*my_answer_list)
+        print("That is correct!")
+        print(gui_bar)
+        index += 1
+
+    print(my_formatted_text)
+    print("")
+    print("Congratulations, you've won! Another Game?")
+    print("")
+
+
+
     # TODO If answer is incorrect, -1 myGameAttempts, and repeat.  Otherwise, continue.
-    # TODO replace correct answer in myGameQuestion
+    # TODO If correct, replace correct answer in myGame_FillInBlankMessage
     # TODO ask next question/answer till all questions are correctly marched or attempts <= 0
 
 
