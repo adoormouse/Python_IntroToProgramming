31a7010e19d41e13071f32a556f76f71bd454ce7
Evan Daly <adoormouse@tuta.io>
2017-07-27 16:27:50 -0700

    Lesson03:Mini-Project: Secret Message, 10.squashing bug

--- ipnd-starter-code-master/stage_3/lesson_3.2_using_functions/secret_message/rename_files.py
+++ ipnd-starter-code-master/stage_3/lesson_3.2_using_functions/secret_message/rename_files.py
@@ -13,3 +13,18 @@
 #
 
 # Your code here.
+import os
+def rename_files():
+    file_list = os.listdir(r"./prank/")
+    print(file_list)
+    saved_path = os.getcwd()
+
+    print(saved_path)
+
+    os.chdir(r"./prank/")
+
+    for file_name in file_list:
+        trans_table = file_name.maketrans("0123456789", "          ", "0123456789")
+        os.rename(file_name, file_name.translate(trans_table))
+
+rename_files()
