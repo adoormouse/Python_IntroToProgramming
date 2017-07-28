e4a8c6942d075f6c72d054857fe8823678fc6680
Evan Daly <adoormouse@tuta.io>
2017-07-27 17:15:29 -0700

    Lesson04:Mini-Project: Draw Turtles, 06.changing turtule atributes Positioned Turtle shape around tutrle kid

--- IPND_materials/4-Create-a-Movie-Website/2_classes/a_draw_turtles/mindstorms.py
+++ IPND_materials/4-Create-a-Movie-Website/2_classes/a_draw_turtles/mindstorms.py
@@ -8,18 +8,27 @@
 # on the Discussion Forum!
 
 import turtle
-
+turtle_starting_pos = (10, 110)
 offset = 45
 
 
 # Your code here.
 def draw_square():
+    # initialize window
     window = turtle.Screen()
     window.bgpic("iliketurtles.gif")
     window.setup(width=400, height=400, startx=0, starty=0)
     window.title("I Like Turtles")
 
+
+    # initialize Turtle starting position for "Turtle Kid"
     myturtle = turtle.Turtle()
+    myturtle.penup()
+    myturtle.setx(turtle_starting_pos[0])
+    myturtle.sety(turtle_starting_pos[1])
+
+    #draw the shape
+    myturtle.pendown()
     myturtle.speed(2)
     myturtle.shape("turtle")
 
