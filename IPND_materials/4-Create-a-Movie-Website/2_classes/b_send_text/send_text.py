1f21077fd114678f7c04b2277b3319dde2adb647
Evan Daly <adoormouse@tuta.io>
2017-07-27 19:56:57 -0700

    Lesson05:Mini-Project: Classes: Send Text

--- IPND_materials/4-Create-a-Movie-Website/2_classes/b_send_text/send_text.py
+++ IPND_materials/4-Create-a-Movie-Website/2_classes/b_send_text/send_text.py
@@ -6,6 +6,20 @@
 # to send a text message using Twilio, a library we'll
 # download from the Internet and add to Python.
 
-from twilio.rest import TwilioRestClient
 
 # Your code here.
+from twilio.rest import Client
+
+# Your Account SID from twilio.com/console
+account_sid = "blah"
+# Your Auth Token from twilio.com/console
+auth_token  = "wacca wacca"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+99999",
+    from_="+0",
+    body="Hello from Python!")
+
+print(message.sid)
