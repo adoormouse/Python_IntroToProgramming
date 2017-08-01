5f266581096c014d72669bc8075020e86df00bef
Evan Daly <adoormouse@tuta.io>
2017-08-01 10:08:11 -0700

    added variabled for 'No magic numbers' requirement

--- IPND_materials/3-Code-Your-Own-Quiz/project/fill-in-the-blanks.py
+++ IPND_materials/3-Code-Your-Own-Quiz/project/fill-in-the-blanks.py
@@ -7,39 +7,9 @@ __email__ = "adoormouse@tuta.io"
 from enum import Enum
 # Enable debugging by setting 'global_debug = True'
 global_debug = False
+gui_padding = 49
+gui_bar = '=' * gui_padding
 
-# IPND Stage 2 Final Project
-
-# You've built a Mad-Libs game with some help from Sean.
-# Now you'll work on your own game to practice your skills and demonstrate what you've learned.
-
-# For this project, you'll be building a Fill-in-the-Blanks quiz.
-# Your quiz will prompt a user with a paragraph containing several blanks.
-# The user should then be asked to fill in each blank appropriately to complete the paragraph.
-# This can be used as a study tool to help you remember important vocabulary!
-
-# Note: Your game will have to accept user input so, like the Mad Libs generator,
-# you won't be able to run it using Sublime's `Build` feature.
-# Instead you'll need to run the program in Terminal or IDLE.
-# Refer to Work Session 5 if you need a refresher on how to do this.
-
-# To help you get started, we've provided a sample paragraph that you can use when testing your code.
-# Your game should consist of 3 or more levels, so you should add your own paragraphs as well!
-
-# The answer for ___1___ is 'function'. Can you figure out the others?
-
-# We've also given you a file called fill-in-the-blanks.pyc which is a working version of the project.
-# A .pyc file is a Python file that has been translated into "byte code".
-# This means the code will run the same as the original .py file, but when you open it
-# it won't look like Python code! But you can run it just like a regular Python file
-# to see how your code should behave.
-
-# Hint: It might help to think about how this project relates to the Mad Libs generator you built with Sean.
-# In the Mad Libs generator, you take a paragraph and replace all instances of NOUN and VERB.
-# How can you adapt that design to work with numbered blanks?
-
-# If you need help, you can sign up for a 1 on 1 coaching appointment: https://calendly.com/ipnd-1-1/20min/
-gui_bar = '=' * 49
 
 class GameLevels(Enum):
     """Defines game difficulty levels Name and Values for QASet()
@@ -91,9 +61,10 @@ def game_main():
     gamelevels = (list(GameLevels.__members__))
 
     # Loop though game menu till user quits
+    spacing = 13
     while True:
         print(gui_bar)
-        print(" "*13 + "FILL IN THE BLANKS GAME")
+        print(" " * spacing + "FILL IN THE BLANKS GAME")
         print(gui_bar)
 
         selection = menu_selection("Select a difficulty level:", gamelevels)
